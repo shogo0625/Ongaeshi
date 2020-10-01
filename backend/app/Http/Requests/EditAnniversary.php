31ba6827606f5e3cabf024b33970bedcd8291f62
shogo0625 <shogo19940625@gmail.com>
2020-10-01 20:58:16 +0900

    Fixed edit anniversary form request

--- backend/app/Http/Requests/EditAnniversary.php
+++ backend/app/Http/Requests/EditAnniversary.php
@@ -15,28 +15,4 @@ class EditAnniversary extends CreateAnniversary
     {
         return true;
     }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return parent::rules();
-    }
-
-    public function attributes()
-    {
-        return parent::attributes();
-    }
-
-    // CreateAnniversary Requestと記述重複 書き方調べる
-    public function getRemindTime($anniversary, $reminder, $unit)
-    {
-        if ($reminder === null) {
-            return $reminder;
-        }
-        return $unit === 'hours' ? date('Y-m-d H:m:s', strtotime($anniversary . "-${reminder} hour")) : date('Y-m-d H:m:s', strtotime($anniversary . "-${reminder} day"));
-    }
 }
